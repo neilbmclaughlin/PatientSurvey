165a4c931ead9458c8ffa3c2ffe566ffd5fd3bc6
Neil McLaughlin <neil@echelon-solutions.co.uk>
2014-02-24 12:53:44 +0000

    Tidied up display to use bootstrap

--- PatientSurvey/App_Start/RouteConfig.cs
+++ PatientSurvey/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PatientSurvey
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Hospitals", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
